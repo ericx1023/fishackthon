1e82c6a3402b3cfbfc1edad0d8b77147d515896e
Evan Chen <evanchen@Evanteki-MacBook-Air.local>
2018-02-03 13:03:52 +0800

    add result

--- web/backend/static/js/main.js
+++ web/backend/static/js/main.js
@@ -5,10 +5,75 @@
 */
 
 (function ($) {
+
+	function base64ToArrayBuffer (base64) {
+		base64 = base64.replace(/^data\:([^\;]+)\;base64,/gmi, '');
+		var binaryString = atob(base64);
+		var len = binaryString.length;
+		var bytes = new Uint8Array(len);
+		for (var i = 0; i < len; i++) {
+			bytes[i] = binaryString.charCodeAt(i);
+		}
+		return bytes.buffer;
+	}
+	var input = document.getElementById('files');
+	var files = input.files;
+	
+
 	function handleFileSelect(evt) {
+		var files = evt.target.files; // FileList object
+
+		// Loop through the FileList and render image files as thumbnails.
+		for (var i = 0, f; f = files[i]; i++) {
+
+			// Only process image files.
+			if (!f.type.match('image.*')) {
+				continue;
+			}
+
+			var reader = new FileReader();
+
+			// Closure to capture the file information.
+			reader.onload = (function (theFile) {
+
+				return function (e) {
+	
+					var span = document.createElement('span');
+					span.innerHTML = ['<img class="thumb" width="100%" src="', e.target.result,
+						'" title="', escape(theFile.name), '"/>'].join('');
+					$('#list').html(span);
+
+				};
+			})(f);
+            reader.onloadend = function (e) {
+                // get EXIF data
+                var exif = EXIF.readFromBinaryFile(base64ToArrayBuffer( e.target.result));
+
+                var lat = exif.GPSLatitude;
+                var lon = exif.GPSLongitude;
+
+                //Convert coordinates to WGS84 decimal
+                var latRef = exif.GPSLatitudeRef || "N";  
+                var lonRef = exif.GPSLongitudeRef || "W";  
+                lat = (lat[0] + lat[1]/60 + lat[2]/3600) * (latRef == "N" ? 1 : -1);  
+                lon = (lon[0] + lon[1]/60 + lon[2]/3600) * (lonRef == "W" ? -1 : 1); 
+                
+               //Send the coordinates to your map
+                Map.AddMarker(lat,lon);
+            } 
+
+			// Read in the image file as a data URL.
+			reader.readAsDataURL(f);
+
+
+			$('html, body').animate({ scrollTop: $("#footer").offset().top }, 1000);
+
+		}
+
 
 		var form = $('form')[0];
 		var formdata = new FormData(form);
+
 		$.ajax({
 			url: "upload",
 			type: "POST",
@@ -20,35 +85,8 @@
 				console.log(resp);
 				if (resp.filename) {
 					alert('上傳成功');
-					var files = evt.target.files; // FileList object
 
-					// Loop through the FileList and render image files as thumbnails.
-					for (var i = 0, f; f = files[i]; i++) {
-
-						// Only process image files.
-						if (!f.type.match('image.*')) {
-							continue;
-						}
-
-						var reader = new FileReader();
-
-						// Closure to capture the file information.
-						reader.onload = (function (theFile) {
-							return function (e) {
-								// Render thumbnail.
-								var span = document.createElement('span');
-								span.innerHTML = ['<img class="thumb" width="100%" src="', e.target.result,
-									'" title="', escape(theFile.name), '"/>'].join('');
-								$('#list').html(span);
-
-							};
-						})(f);
-
-						// Read in the image file as a data URL.
-						reader.readAsDataURL(f);
-						$('html, body').animate({ scrollTop: $("#footer").offset().top }, 1000);
-
-					}
+				
 				}
 			},
 			error: function (jqXHR, textStatus, errorMessage) {
@@ -60,6 +98,8 @@
 	}
 
 	document.getElementById('files').addEventListener('change', handleFileSelect, false);
+	
+
 
 	skel.breakpoints({
 		xlarge: '(max-width: 1680px)',
@@ -204,8 +244,14 @@
 			})
 			.done(function(res){
 				console.log(res)
+				updateResult();
 			})
 		}
 	})
 
+	function updateResult(res) {
+		$('.js-fishname').html(res.fishname);
+		$('.js-allow').html(res.allow);
+		
+	}
 })(jQuery);
